kaustav-net/ti-uboot

spi: cadence-qspi: Add support to get clock rate from clk framework
Use clock APIs to get rate of QSPI ref clk

Signed-off-by: Vignesh R <vigneshr@ti.com>

drivers/spi/cadence_qspi.c
@@ -5,6 +5,7 @@
  */
 
 #include <common.h>
+#include <clk.h>
 #include <dm.h>
 #include <fdtdec.h>
 #include <malloc.h>
@@ -18,6 +19,10 @@
 #define CQSPI_READ			2
 #define CQSPI_WRITE			3
 
+#ifndef CONFIG_CQSPI_REF_CLK
+#define CONFIG_CQSPI_REF_CLK  0
+#endif
+
 DECLARE_GLOBAL_DATA_PTR;
 
 static int cadence_spi_write_speed(struct udevice *bus, uint hz)
@@ -26,10 +31,10 @@ static int cadence_spi_write_speed(struct udevice *bus, uint hz)
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
 
 	cadence_qspi_apb_config_baudrate_div(priv->regbase,
-					     CONFIG_CQSPI_REF_CLK, hz);
+					     priv->ref_clk_hz, hz);
 
 	/* Reconfigure delay timing if speed is changed. */
-	cadence_qspi_apb_delay(priv->regbase, CONFIG_CQSPI_REF_CLK, hz,
+	cadence_qspi_apb_delay(priv->regbase, priv->ref_clk_hz, hz,
 			       plat->tshsl_ns, plat->tsd2d_ns,
 			       plat->tchsh_ns, plat->tslch_ns);
 
@@ -162,9 +167,23 @@ static int cadence_spi_probe(struct udevice *bus)
 {
 	struct cadence_spi_platdata *plat = bus->platdata;
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
+	long int clk_rate = 0;
 
 	priv->regbase = plat->regbase;
 	priv->ahbbase = plat->ahbbase;
+
+	if (plat->clk.dev)
+		clk_rate = clk_get_rate(&plat->clk);
+
+	if (clk_rate <= 0) {
+		clk_rate = dev_read_u32_default(bus, "clock-frequency",
+						CONFIG_CQSPI_REF_CLK);
+		if (!clk_rate) {
+			printf("cqspi: refclk frequency unavailable\n");
+			return -EINVAL;
+		}
+	}
+	priv->ref_clk_hz = clk_rate;
 
 	if (!priv->qspi_is_init) {
 		cadence_qspi_apb_controller_init(plat);
@@ -263,6 +282,7 @@ static int cadence_spi_ofdata_to_platdata(struct udevice *bus)
 	int node = dev_of_offset(bus);
 	fdt_addr_t mmap_addr, mmap_size;
 	int subnode;
+	int ret;
 
 	plat->regbase = (void *)devfdt_get_addr_index(bus, 0);
 	plat->ahbbase = (void *)devfdt_get_addr_index(bus, 1);
@@ -277,6 +297,12 @@ static int cadence_spi_ofdata_to_platdata(struct udevice *bus)
 	plat->fifo_width = fdtdec_get_uint(blob, node, "cdns,fifo-width", 4);
 	plat->trigger_address = fdtdec_get_uint(blob, node,
 						"cdns,trigger-address", 0);
+
+	ret = clk_get_by_index(bus, 0, &plat->clk);
+	if (ret && ret != -ENOENT && ret != -ENODEV && ret != -ENOSYS) {
+		debug("cqspi: failed to get clock\n");
+		return ret;
+	}
 
 	/* All other paramters are embedded in the child node */
 	subnode = fdt_first_subnode(blob, node);

r-vignesh/u-boot

Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

drivers/spi/cadence_qspi.c
@@ -5,6 +5,7 @@
  */
 
 #include <common.h>
+#include <clk.h>
 #include <dm.h>
 #include <fdtdec.h>
 #include <malloc.h>
@@ -20,16 +21,20 @@
 #define CQSPI_READ			2
 #define CQSPI_WRITE			3
 
+#ifndef CONFIG_CQSPI_REF_CLK
+#define CONFIG_CQSPI_REF_CLK  0
+#endif
+
 static int cadence_spi_write_speed(struct udevice *bus, uint hz)
 {
 	struct cadence_spi_platdata *plat = bus->platdata;
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
 
 	cadence_qspi_apb_config_baudrate_div(priv->regbase,
-					     CONFIG_CQSPI_REF_CLK, hz);
+					     priv->ref_clk_hz, hz);
 
 	/* Reconfigure delay timing if speed is changed. */
-	cadence_qspi_apb_delay(priv->regbase, CONFIG_CQSPI_REF_CLK, hz,
+	cadence_qspi_apb_delay(priv->regbase, priv->ref_clk_hz, hz,
 			       plat->tshsl_ns, plat->tsd2d_ns,
 			       plat->tchsh_ns, plat->tslch_ns);
 
@@ -162,6 +167,7 @@ static int cadence_spi_probe(struct udevice *bus)
 {
 	struct cadence_spi_platdata *plat = bus->platdata;
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
+	long int clk_rate = 0;
 	int ret;
 
 	priv->regbase = plat->regbase;
@@ -172,6 +178,19 @@ static int cadence_spi_probe(struct udevice *bus)
 		dev_warn(bus, "Can't get reset: %d\n", ret);
 	else
 		reset_deassert_bulk(&priv->resets);
+
+	if (plat->clk.dev)
+		clk_rate = clk_get_rate(&plat->clk);
+
+	if (clk_rate <= 0) {
+		clk_rate = dev_read_u32_default(bus, "clock-frequency",
+						CONFIG_CQSPI_REF_CLK);
+		if (!clk_rate) {
+			debug("cqspi: refclk frequency unavailable\n");
+			return -EINVAL;
+		}
+	}
+	priv->ref_clk_hz = clk_rate;
 
 	if (!priv->qspi_is_init) {
 		cadence_qspi_apb_controller_init(plat);
@@ -264,6 +283,7 @@ static int cadence_spi_ofdata_to_platdata(struct udevice *bus)
 {
 	struct cadence_spi_platdata *plat = bus->platdata;
 	ofnode subnode;
+	int ret;
 
 	plat->regbase = (void *)devfdt_get_addr_index(bus, 0);
 	plat->ahbbase = (void *)devfdt_get_addr_size_index(bus, 1,
@@ -277,6 +297,12 @@ static int cadence_spi_ofdata_to_platdata(struct udevice *bus)
 	/* Use DAC mode only when MMIO window is at least 8M wide */
 	if (plat->ahbsize >= SZ_8M)
 		plat->use_dac_mode = true;
+
+	ret = clk_get_by_index(bus, 0, &plat->clk);
+	if (ret && ret != -ENOENT && ret != -ENODEV && ret != -ENOSYS) {
+		debug("cqspi: failed to get clock\n");
+		return ret;
+	}
 
 	/* All other paramters are embedded in the child node */
 	subnode = dev_read_first_subnode(bus);